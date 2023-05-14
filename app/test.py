45788e11a68fdecbe84cf20f64bd90fc8437eb96
luist@psma.com.au <qq97qf24ffL>
2023-05-14 22:57:56 +1000

    fixing unit test

--- app/test.py
+++ app/test.py
@@ -16,14 +16,14 @@ class TestAddNumbers(unittest.TestCase):
         
     def test_add_negative_numbers(self):
         self.assertEqual(add_numbers(-3, -5), -8)
-        self.assertEqual(add_numbers(-10, -20), -31)
+        self.assertEqual(add_numbers(-10, -20), -30)
 
     def test_add_decimal_numbers(self):
         self.assertAlmostEqual(add_numbers(3.14, 1.59), 4.73, places=2)
         self.assertAlmostEqual(add_numbers(0.1, 0.2), 0.3, places=1)
         
     def test_add_large_numbers(self):
-        self.assertEqual(add_numbers(1000000000, 2000000000), 300000000000)
+        self.assertEqual(add_numbers(1000000000, 2000000000), 3000000000)
         self.assertEqual(add_numbers(-999999999, -999999998), -1999999997)
 
 if __name__ == '__main__':
